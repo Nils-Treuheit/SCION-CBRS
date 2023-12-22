6167ee7e526e6fc2864b3c0fd32e0678a8639001
Nils-Treuheit <nils.treuheit@st.ovgu.de>
2023-12-22 18:14:24 +0100

    clean up

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # SCION-CDN
-Create a Benchmark to measure the impact of different communication paths on the content delivery of an emulated SCION network using the SEED-Emulator 
-Content Files are purpose-oriented deviations of cited sources to benchmark different content network traffic in the SEED-Emulation. Content files will only be temporarily available to transport them between devices and will be replaced by links very soon as they should not be redistributed on the web.
+Create a Benchmark to measure the impact of different communication paths on the content delivery of small SCION network using 2 ASes with several paths and hops between them. 
+Content Files are purpose-oriented deviations of cited sources to benchmark different content network traffic. Content files will not be uploaded to the repository and might only be temporarily available under the sourced links as I do not plan to regular update them and have to refrain from redistributing content of other creators on the web. The website text is partially generated with ChatGPT and not fact checked as I just needed a larger chunk of text to download. 
--- servers.go
+++ servers.go
@@ -1,22 +1,16 @@
-/* Server structures from my own SCION_TV Project (https://github.com/Nils-Treuheit/SCION-TV) 
-   based on Marten Gartner's work in scion-apps (https://github.com/netsec-ethz/scion-apps/tree/master/_examples/shttp) */
-
 package main
+
+/* Server structures from my own SCION_TV Project (https://github.com/Nils-Treuheit/SCION-TV)
+   based on Marten Gartner's work in scion-apps (https://github.com/netsec-ethz/scion-apps/tree/master/_examples/shttp) */
 
 import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
 	"time"
-	"path/filepath"
-	"strings"
-	"crypto/tls"
-	"net/http/httputil"
-	"net/url"
 
 	"github.com/gorilla/handlers"
 	"github.com/netsec-ethz/scion-apps/pkg/shttp"
@@ -24,27 +18,21 @@ import (
 
 // relatively simple webserver fileserver topology
 // derived from the SCION-TV project
-func main() 
-{
-	local := flag.String("local", "0.0.0.0:8899", "The local HTTP or SCION address on which the server will be listening")
-	remote := flag.String("remote", "19-ffaa:1:bcc,[127.0.0.1]:9988", "The SCION or HTTP address on which the server will be requested")
+func main() {
 	webServPort := flag.String("webServPort", "80", "The port to serve website on")
 	tslServPort := flag.String("tslServPort", "433", "The tsl port to serve website with tsl certificate on")
-	webDir := flag.String("webDir", "html", "The directory of static webpage elements to host")
-	fileServPort := flag.String("fileServPort", "8080", "The port to serve website on")
-	fileDir := flag.String("fileDir", "hls", "The directory of streaming content to host")
+	webDir := flag.String("webDir", "website", "The directory of static webpage elements to host")
+	fileServPort := flag.String("fileServPort", "8899", "The port to serve website on")
+	fileDir := flag.String("fileDir", "stream_files", "The directory of streaming content to host")
 	flag.Parse()
 
-	go file_server(fileDir,fileServPort)
-	go web_server(webDir,tslServPort,webServPort)
-	proxy(local,remote)
+	go file_server(fileDir, fileServPort)
+	web_server(webDir, tslServPort, webServPort)
 }
 
 // addHeaders will act as middleware to give us CORS support
-func addHeaders(h http.Handler) http.HandlerFunc 
-{
-	return func(w http.ResponseWriter, r *http.Request) 
-	{
+func addHeaders(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		h.ServeHTTP(w, r)
 	}
@@ -52,28 +40,26 @@ func addHeaders(h http.Handler) http.HandlerFunc
 
 /*
 This is a very simple static file server in go
-Navigating to http://localhost:8080 will display the directory file listings.
+Navigating to http://localhost:8899 will display the directory file listings.
 */
-func file_server(directory *string, port *string) 
-{
+func file_server(directory *string, port *string) {
 	// Sample video from https://www.youtube.com/watch?v=xj2heO4-u-8
 	mux := http.NewServeMux()
 	mux.Handle("/", addHeaders(http.FileServer(http.Dir(*directory))))
 
-	log.Printf("File-Server serves %s folder's streaming content on HTTP port: %s\n", *directory, *port )
-	log.Fatalf("%s", http.ListenAndServe(":"+*port, mux))
+	log.Printf("File-Server serves %s folder's streaming content on HTTP port: %s\n", *directory, *port)
+	log.Fatalf("%s", shttp.ListenAndServe(":"+*port, mux))
 }
 
 /*
 This is a very simple webpage server in go
 Navigating to https://localhost:433 or http://localhost:80 will display the index.html.
 */
-func web_server(webDir *string, tslPort *string, webPort *string)
-{
+func web_server(webDir *string, tslPort *string, webPort *string) {
 	webpage := "index.html"
-	website := *webDir+"/"+webpage
-	icon := *webDir+"/favicon.ico"
-	pic := *webDir+"/background.png"
+	website := *webDir + "/" + webpage
+	icon := *webDir + "/favicon.ico"
+	pic := *webDir + "/background.png"
 
 	certFile := flag.String("cert", "", "Path to TLS server certificate for optional https")
 	keyFile := flag.String("key", "", "Path to TLS server key for optional https")
@@ -81,74 +67,67 @@ func web_server(webDir *string, tslPort *string, webPort *string)
 
 	m := http.NewServeMux()
 
-	m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){http.ServeFile(w, r, website)})
-	m.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request){http.ServeFile(w, r, icon)})
-	m.HandleFunc("/background.png", func(w http.ResponseWriter, r *http.Request){http.ServeFile(w, r, pic)})
-
+	m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, website) })
+	m.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, icon) })
+	m.HandleFunc("/background.png", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, pic) })
 
 	// handler that responds with a friendly greeting
-	m.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) 
-	  {
+	m.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
 		// Status 200 OK will be set implicitly
 		w.Header().Set("Content-Type", "text/plain")
 		_, _ = w.Write([]byte(`Hello World! I am a dummy website to test the impact of SCION path selection on the performance of content delivery.`))
-	  }
-	)
+	})
+
+	// handler that responds with a chatGPT story
+	m.HandleFunc("/sample-text", func(w http.ResponseWriter, r *http.Request) {
+		// Status 200 OK will be set implicitly
+		w.Header().Set("Content-Type", "text/plain")
+		_, _ = w.Write([]byte(`Here a Chat GPT Story about the history of the Internet\n\n\nOnce upon a time, in the early days of the internet, connections were slow, and the digital world was a far cry from the intricate tapestry it is today. It was a time when the concept of "surfing the web" meant navigating through simple HTML pages, and the idea of streaming high-quality video or conducting seamless online transactions was but a distant dream.\n\nAs the years passed, a wave of advancements in internet technologies began to reshape the landscape. The first significant milestone was the advent of broadband connections, replacing the screeching sounds of dial-up modems with high-speed, always-on connectivity. This transition marked the beginning of a new era, enabling users to explore richer content and engage in more complex online activities.\n\nThe rise of Web 2.0 brought forth a paradigm shift. The internet ceased to be a static collection of pages and transformed into a dynamic platform for collaboration and interaction. Social media platforms emerged, connecting people across the globe in ways previously unimaginable. The internet was no longer just a source of information; it became a virtual social space where ideas, opinions, and cat videos could be shared instantaneously.\n\nSimultaneously, the field of e-commerce flourished. Secure online transactions became a reality, paving the way for the digital marketplace. Consumers could now browse, shop, and pay for goods and services without leaving the comfort of their homes. This not only revolutionized retail but also laid the foundation for a globalized economy where borders mattered less in the digital realm.\n\nThe internet's infrastructure also underwent significant improvements. Fiber-optic cables replaced traditional copper wiring, dramatically increasing data transfer speeds and bandwidth. The concept of cloud computing emerged, allowing users to store and access vast amounts of data remotely. This not only streamlined processes for individuals and businesses but also contributed to the rise of services such as online storage, streaming, and collaborative platforms.\n\nThe proliferation of mobile devices further fueled the internet's evolution. Smartphones became ubiquitous, bringing the internet into the palms of billions. Mobile applications transformed how we access information, communicate, and even manage our daily lives. The mobile revolution made the internet more accessible and personalized, blurring the lines between the digital and physical worlds.\n\nThe Internet of Things (IoT) emerged as yet another frontier. Everyday objects became "smart," equipped with sensors and connectivity, creating a vast network of interconnected devices. Homes, cities, and industries became more efficient and interconnected, laying the groundwork for a future where the internet seamlessly integrates into every aspect of our lives.\n\nAs we venture into the era of 5G connectivity, the internet's journey continues to unfold. Faster speeds, lower latency, and increased capacity promise a world where immersive experiences, such as augmented reality and virtual reality, become commonplace. The boundaries of what is possible on the internet continue to expand, with innovations like blockchain technology and artificial intelligence shaping its future.\n\nIn this ongoing saga of technological progress, the internet stands not just as a tool for information but as a dynamic force driving societal, economic, and cultural change. The journey is far from over, and as we navigate the ever-evolving landscape of internet technologies, the next chapter promises even more remarkable advancements and transformative possibilities.\n\nAmidst the evolving tapestry of internet technologies, a new chapter unfolds with the introduction of the Secure Internet Architecture, better known as SCION. In the quest for a safer and more efficient network, SCION emerges as a beacon of innovation, reshaping the very foundations of how data traverses the digital realm.\n\nSCION, born from the collective wisdom of internet pioneers, represents a paradigm shift in network architecture. Unlike traditional protocols, SCION abandons the one-size-fits-all approach in favor of a modular and flexible design. It introduces a novel concept of path-aware networking, allowing data packets to navigate through the most secure and efficient routes available.\n\nSecurity lies at the heart of SCION's transformative power. In a landscape where cyber threats loom large, SCION pioneers a departure from conventional security measures. Its inherent design mitigates risks associated with common vulnerabilities, such as route hijacking and man-in-the-middle attacks. By introducing a secure-by-design framework, SCION erects a formidable defense against the ever-evolving cyber threats that plague the internet.\n\nOne of the groundbreaking features of SCION is its ability to provide provable security guarantees. Traditional protocols often rely on assumptions, leaving room for uncertainties in the face of sophisticated attacks. SCION, on the other hand, employs a principled approach backed by mathematical proofs. This not only enhances the resilience of the network but instills confidence in users and organizations relying on its infrastructure.\n\nEfficiency is another frontier where SCION excels. Through its innovative path selection mechanism, SCION optimizes data transfer, minimizing latency and ensuring swift delivery. This efficiency is a game-changer for applications demanding real-time responsiveness, such as online gaming, video conferencing, and emerging technologies like the Internet of Things (IoT).\n\nAs SCION gains traction, it heralds a new era for the internet—a safer, more reliable, and efficient era. Industries, governments, and individuals alike recognize its potential to reshape digital landscapes, fortifying critical infrastructure and safeguarding sensitive information.\n\nGovernments find in SCION a trustworthy foundation for securing national communications, protecting against cyber threats that jeopardize national security. Businesses, eager to fortify their digital presence, embrace SCION as a catalyst for innovation, ensuring a resilient and secure environment for their operations.\n\nFor individuals navigating the digital realm, SCION becomes a guarantee of privacy and reliability. With the assurance that their data travels through the most secure channels, users can engage in online activities without the constant worry of unauthorized access or data breaches.\n\nAs SCION weaves its way into the fabric of the internet, its impact reverberates across continents and industries. The internet, once a realm of uncertainties, transforms into a secure and efficient network, fostering innovation, collaboration, and progress. SCION's journey is not just a technological evolution; it's a testament to the relentless pursuit of a safer and more interconnected world in the digital age. And so, the story of the internet's metamorphosis continues, guided by the transformative power of SCION.`))
+	})
 
 	// handler that responds with an image file
-	m.HandleFunc("/sample-image", func(w http.ResponseWriter, r *http.Request) 
-	  {
+	m.HandleFunc("/sample-image", func(w http.ResponseWriter, r *http.Request) {
 		// serve the sample JPG file
 		// Status 200 OK will be set implicitly
 		// Content-Length will be inferred by server
 		// Content-Type will be detected by server
-		http.ServeFile(w, r, *webDir+"SCION.JPG") 
+		http.ServeFile(w, r, *webDir+"/SCION.JPG")
 		// Sample image from https://blog.apnic.net/wp-content/uploads/2021/09/SCION-FT-555x202.jpg?v=1670e3759db62840c91aa22608946e73
-	  }
-	)
-	
+	})
+
 	// handler that responds with an gif file
-	m.HandleFunc("/sample-gif", func(w http.ResponseWriter, r *http.Request) 
-	  {
+	m.HandleFunc("/sample-gif", func(w http.ResponseWriter, r *http.Request) {
 		// serve the sample GIF file
 		// Status 200 OK will be set implicitly
 		// Content-Length will be inferred by server
 		// Content-Type will be detected by server
-		http.ServeFile(w, r, *webDir+"boycott.gif") 
+		http.ServeFile(w, r, *webDir+"/boycott.gif")
 		// Sample image from https://giphy.com/gifs/southparkgifs-3o6ZsZTFpJxmZaeqcw
-	  }
-	)
+	})
 
 	// handler that responds with an audio file
-	m.HandleFunc("/sample-audio", func(w http.ResponseWriter, r *http.Request) 
-	  {
+	m.HandleFunc("/sample-audio", func(w http.ResponseWriter, r *http.Request) {
 		// serve the sample AUDIO file
 		// Status 200 OK will be set implicitly
 		// Content-Length will be inferred by server
 		// Content-Type will be detected by server
-		http.ServeFile(w, r, *webDir+"Chopin-nocturne-op-9-no-2.mp3") 
+		http.ServeFile(w, r, *webDir+"/Chopin-nocturne-op-9-no-2.mp3")
 		// Sample music from https://orangefreesounds.com/chopin-nocturne-op-9-no-2
-	  }
-	)
+	})
 
 	// handler that responds with an video file
-	m.HandleFunc("/sample-video", func(w http.ResponseWriter, r *http.Request) 
-	  {
+	m.HandleFunc("/sample-video", func(w http.ResponseWriter, r *http.Request) {
 		// serve the sample MP4 file
 		// Status 200 OK will be set implicitly
 		// Content-Length will be inferred by server
 		// Content-Type will be detected by server
-		http.ServeFile(w, r, *webDir+"SCION_DDoS_Def.mp4") 
+		http.ServeFile(w, r, *webDir+"/SCION_DDoS_Def.mp4")
 		// Sample video from https://www.youtube.com/watch?v=-JeEppbCZTw
-	  }
-	)
-	
+	})
 
 	// GET handler that responds with some json data
-	m.HandleFunc("/sample-json", func(w http.ResponseWriter, r *http.Request) 
-	  {
-		if r.Method == http.MethodGet 
-		{
+	m.HandleFunc("/sample-json", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
 			data := struct {
 				Time    string
 				Agent   string
@@ -163,83 +142,34 @@ func web_server(webDir *string, tslPort *string, webPort *string)
 			resp, _ := json.Marshal(data)
 			w.Header().Set("Content-Type", "application/json")
 			fmt.Fprint(w, string(resp))
-		} 
-		else { http.Error(w, "wrong method: "+r.Method, http.StatusForbidden) }
-	  }
-	)
+		} else {
+			http.Error(w, "wrong method: "+r.Method, http.StatusForbidden)
+		}
+	})
 
 	// POST handler that responds by parsing form values and returns them as string
-	m.HandleFunc("/form", func(w http.ResponseWriter, r *http.Request) 
-	  {
-		if r.Method == http.MethodPost 
-		{
-			if err := r.ParseForm(); err != nil 
-			{
+	m.HandleFunc("/form", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			if err := r.ParseForm(); err != nil {
 				http.Error(w, "invalid form data", http.StatusBadRequest)
 				return
 			}
 			w.Header().Set("Content-Type", "text/plain")
 			fmt.Fprint(w, "received following data:\n")
-			for s := range r.PostForm 
-			{ fmt.Fprint(w, s, "=", r.PostFormValue(s), "\n") }
-		} 
-		else { http.Error(w, "wrong method: "+r.Method, http.StatusForbidden) }
-	  }
-	)
+			for s := range r.PostForm {
+				fmt.Fprint(w, s, "=", r.PostFormValue(s), "\n")
+			}
+		} else {
+			http.Error(w, "wrong method: "+r.Method, http.StatusForbidden)
+		}
+	})
 
 	handler := handlers.LoggingHandler(os.Stdout, m)
-	if *certFile != "" && *keyFile != "" 
-	{
+	if *certFile != "" && *keyFile != "" {
 		go func() { log.Fatal(shttp.ListenAndServeTLS(":"+*tslPort, *certFile, *keyFile, handler)) }()
 		log.Printf("Web-Server serves %s webpage and its elements on HTTP port: %s\n", webpage, *tslPort)
+	} else {
+		log.Printf("Web-Server serves %s webpage and its elements on HTTP port: %s\n", webpage, *webPort)
 	}
-	else { log.Printf("Web-Server serves %s webpage and its elements on HTTP port: %s\n", webpage, *webPort) }
 	log.Fatal(shttp.ListenAndServe(":"+*webPort, handler))
-}
-
-
-/*
-PLEASE NOTE:
-------------
-This proxy implementation is pretty much a copy of the proxy implementation from the official scion-apps repository[https://github.com/netsec-ethz/scion-apps]
-The current implementation of the official proxy can be found here[https://github.com/netsec-ethz/scion-apps/blob/master/_examples/shttp/proxy/main.go].
-There will be differences between mine and the linked implementation since the repositories are not synced.
-
-This proxy is used as a SCION to IP bridge in order to make a broadcasted Web-Content available to the local machine/network.
-The proxy will be used as a SCION ingress proxy.
-
-Navigating to http://localhost:8899 to access the streamed content.
-*/
-func proxy(local *string, remote *string) {
-
-	mux := http.NewServeMux()
-
-	// parseUDPAddr validates if the address is a SCION address
-	// which we can use to proxy to SCION
-	if _, err := snet.ParseUDPAddr(*remote); err == nil {
-		proxyHandler, err := shttp.NewSingleSCIONHostReverseProxy(*remote, &tls.Config{InsecureSkipVerify: true, NextProtos: []string{"h3"}})
-		if err != nil {
-			log.Fatalf("Failed to create SCION reverse proxy %s", err)
-		}
-
-		mux.Handle("/", proxyHandler)
-		log.Printf("Proxy connected to SCION remote %s\n", *remote)
-	} else {
-		u, err := url.Parse(*remote)
-		if err != nil {
-			log.Fatal(fmt.Sprintf("Failed parse remote %s, %s", *remote, err))
-		}
-		log.Printf("Proxy connected to HTTP remote %s\n", *remote)
-		mux.Handle("/", httputil.NewSingleHostReverseProxy(u))
-	}
-
-	if lAddr, err := snet.ParseUDPAddr(*local); err == nil {
-		log.Printf("Proxy listens on SCION %s\n", *local)
-		// ListenAndServe does not support listening on a complete SCION Address,
-		// Consequently, we only use the port (as seen in the server example)
-		log.Fatalf("%s", shttp.ListenAndServe(fmt.Sprintf(":%d", lAddr.Host.Port), mux, nil))
-	} else {
-		log.Printf("Proxy listens on HTTP %s\n", *local)
-		log.Fatalf("%s", http.ListenAndServe(*local, mux))
-	}
 }
