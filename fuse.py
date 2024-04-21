633f2ecf9db0e805cba7d09a7c4516d36a82ff95
Nils-Treuheit <nils.treuheit@st.ovgu.de>
2024-04-21 11:52:29 +0200

    delete temp script

--- fuse.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-data1 = pd.read_csv("seq_fetch_times.csv", index_col=0)
-data2 = pd.read_csv("./fetch_benchmarks/noRepSel-seq_fetch_times_part1.csv", index_col=0)
-data = (data1 + data2)/2
-csv_file = "noRepSel-seq_fetch_times.csv"
-data.to_csv(csv_file)
